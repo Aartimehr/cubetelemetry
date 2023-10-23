d9326c51744eb4145debac7b8c201e2ae96d10a8
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-10-23 10:41:27 -0700

    Create product.route.js

---
+++ backend/product.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+ router.get('/',(req,res,next) => {
+    res.send('getting a list of all products......');
+ });
+
+ router.post('/',(req,res,next) => {
+    res.send('product created');
+ });
+
+ module.exports = router;
