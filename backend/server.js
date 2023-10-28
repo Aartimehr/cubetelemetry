dfa009a95d7703bccea92d7b22710fe8bba7e901
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-10-28 05:28:53 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,53 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose') 
 
-const ProductRoute = require('./Routes/Products.route')
-app.use('/products',ProductRoute);
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}))
+
+mongoose.connect('mongodb+srv://mehra11aarti:7827351031@cluster0.tjprssr.mongodb.net/?retryWrites=true&w=majority',  
+  { 
+    dbName:'Telemetry',
+    user:'mehra11aarti',
+    pass: '7827351031',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    
+  })
+
+.then(() => {
+    console.log('mongodb connected....');
+});
+
+app.all('/test', (req,res)   =>  {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 
-app.listen(3000,()=> { 
-    console.log('Node API app is running on port 3000')
-})
- 
+const ProductRoute = require('./Routes/product.route');
+app.use('/products', ProductRoute);
+
+
+app.use((req,res,next)  => {
+    const err = new Error("Not found");
+    err.status=404
+    next(err);
+});
+
+
+//error handler
+app.use((err,req,res,next)  => {
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500 ,
+            message: err.message
+        }
+    });
+   
+});
+app.listen(3000,() => {
+    console.log("node api app is running on port 3000");
+
+}); 
